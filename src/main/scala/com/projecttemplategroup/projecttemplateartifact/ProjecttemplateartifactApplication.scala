92f90bc2bbb7ba8be6293090c96030de60911364
victor <victorpaul.war@gmail.com>
2020-09-10 11:14:19 +0300

    chanegd configs

--- src/main/scala/com/projecttemplategroup/projecttemplateartifact/ProjecttemplateartifactApplication.scala
+++ src/main/scala/com/projecttemplategroup/projecttemplateartifact/ProjecttemplateartifactApplication.scala
@@ -2,8 +2,9 @@ package com.projecttemplategroup.projecttemplateartifact
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration
 
-@SpringBootApplication
+@SpringBootApplication(exclude = Array(classOf[SecurityAutoConfiguration])) // we do not need default spring boot security as we prefer to use pac4j
 class ProjecttemplateartifactApplication
 
 object ProjecttemplateartifactApplication extends App {
